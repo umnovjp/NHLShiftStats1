62058cbe2f5b167f71ce3ccb698a1da88401dae7
umnovjp <umnovjp@yahoo.com>
2023-07-31 23:36:03 -0500

    minor chnages to script again some changes to map

--- assets/script.js
+++ assets/script.js
@@ -360,8 +360,8 @@ function getInputValue() {
                   if ((realFiveOnFive[0][2 * i] === realFiveOnFive[1][2 * j]) && (realFiveOnFive[0][2 * i + 1] === realFiveOnFive[1][2 * j + 1]))  // 
                   {  
                 countHome = 0; countAway = 0;
-                for (k = i; k < realFiveOnFive[0].length / 2; k++) {if (realFiveOnFive[0][2 * k] === realFiveOnFive[0][2 * i]) {countHome++}}
-                for (l = j; l < realFiveOnFive[1].length / 2; l++) {if (realFiveOnFive[1][2 * l] === realFiveOnFive[1][2 * j]) {countAway++}}
+                for (k = 0; k < realFiveOnFive[0].length / 2; k++) {if (realFiveOnFive[0][2 * k] === realFiveOnFive[0][2 * i]) {countHome++}}
+                for (l = 0; l < realFiveOnFive[1].length / 2; l++) {if (realFiveOnFive[1][2 * l] === realFiveOnFive[1][2 * j]) {countAway++}}
                 console.log('mutual penalty again', i, j, countHome, countAway)
               }
                 }
